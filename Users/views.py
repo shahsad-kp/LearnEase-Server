8b08017f9a0890692d3e22744a606fc522af83e6
shahsad <shahsadkpklr@gmail.com>
2023-06-23 18:49:00 +0530

    added access and refresh token to register view

--- Users/views.py
+++ Users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from Users.models import User
 from Users.serializers import UserSerializer
@@ -13,6 +14,21 @@ class UserRegisterView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        if response.status_code == 201:
+            serializer = self.get_serializer(data=request.data)
+            serializer.is_valid()
+            user = User.objects.get(email=serializer.data['email'])
+            refresh = RefreshToken.for_user(user)
+            data = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+                'user': UserSerializer(user).data
+            }
+            response.data = data
+        return response
 
 
 class UpdateUserView(UpdateAPIView):
