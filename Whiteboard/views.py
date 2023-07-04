3d67635347655bde56933d8ff3bfa8a5046c2928
shahsad <shahsadkpklr@gmail.com>
2023-07-04 14:13:44 +0530

    added serializer

--- Whiteboard/views.py
+++ Whiteboard/views.py
@@ -2,10 +2,11 @@ from rest_framework.generics import RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from Whiteboard.models import Whiteboard
+from Whiteboard.serializers import WhiteboardSerializer
 
 
 class GetWhiteBoardData(RetrieveAPIView):
-    serializer_class = None
+    serializer_class = WhiteboardSerializer
     permission_classes = [IsAuthenticated]
     lookup_field = 'room_id'
     lookup_url_kwarg = 'room_id'
