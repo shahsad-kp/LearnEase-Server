431d03d3577bd02a5b59baf0f3696490d07a28a9
shahsad <shahsadkpklr@gmail.com>
2023-07-21 20:11:46 +0530

    added debug info in env

--- learn_ease_backend/settings.py
+++ learn_ease_backend/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG') == 'True'
 
 ALLOWED_HOSTS = [env('ALLOWED_HOSTS').split(',')]
 
