cd34b44ed3ddc8858695196af4279ebdfbbc91ab
shahsad <shahsadkpklr@gmail.com>
2023-08-23 20:24:31 +0530

    feature: Modified UserRegister view to send email verification
    - Created email_verification_template.html
    - Deleted serializer.py from Auth
    - Added isActive field in UserSerializer
    - Removed OtpVerification view
    - Added verification email sending in UserRegisterView

--- Auth/views.py
+++ Auth/views.py
@@ -1,11 +1,15 @@
 from django.contrib.auth import authenticate
+from django.contrib.auth.tokens import default_token_generator
+from django.template.loader import render_to_string
+from django.urls import reverse
+from django.conf import settings
+from django.core.mail import send_mail
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK, HTTP_401_UNAUTHORIZED
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
-from Auth.serializer import OTPSerializer
 
 from Users.models import User
 from Users.serializers import UserSerializer
@@ -54,25 +58,24 @@ class UserRegisterView(CreateAPIView):
             serializer = self.get_serializer(data=request.data)
             serializer.is_valid()
             user = User.objects.get(email=serializer.data['email'])
-            refresh = RefreshToken.for_user(user)
-            data = {
-                'refresh': str(refresh),
-                'access': str(refresh.access_token),
-                'user': UserSerializer(user).data
-            }
-            response.data = data
-        return response
-    
-class OtpVerification(CreateAPIView):
-    serializer_class = OTPSerializer
-    permission_classes = [AllowAny]
-    
-    def post(self, request, *args, **kwargs):
-        response = super().post(request, *args, **kwargs)
-        if response.status_code == 201:
-            serializer = self.get_serializer(data=request.data)
-            serializer.is_valid()
-            user = User.objects.get(email=serializer.data['email'])
+
+            token = default_token_generator.make_token(user)
+            verification_link = request.build_absolute_uri(reverse('verify_email', args=[token]))
+            subject = 'Email Verification'
+            message = render_to_string(
+                'email_verification_template.html',
+                {'verification_link': verification_link}
+            )
+            from_email = settings.EMAIL_HOST_USER
+            to_email = [user.email]
+            send_mail(
+                subject=subject,
+                message='',
+                html_message=message,
+                from_email=from_email,
+                recipient_list=to_email,
+            )
+
             refresh = RefreshToken.for_user(user)
             data = {
                 'refresh': str(refresh),
--- Users/serializers.py
+++ Users/serializers.py
@@ -1,20 +1,22 @@
 from rest_framework.fields import ImageField
-from rest_framework.serializers import ModelSerializer, CharField
+from rest_framework.serializers import ModelSerializer, BooleanField
 
 from Users.models import User
 
 
 class UserSerializer(ModelSerializer):
     profilePicture = ImageField(source='profile_pic', required=False)
+    isActive = BooleanField(source='is_active', required=False)
 
     class Meta:
         model = User
-        fields = ['id', 'name', 'email', 'password', 'profilePicture']
+        fields = ['id', 'name', 'email', 'password', 'profilePicture', 'isActive']
         extra_kwargs = {'password': {'write_only': True, 'required': True}}
 
     def create(self, validated_data):
         user = User(**validated_data)
         user.set_password(validated_data.get('password'))
+        user.is_active = False
         user.save()
         return user
 
